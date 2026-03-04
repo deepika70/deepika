4a64d01c67177840a5b756c6251e7a030aab53d7
Deepika <ragipindideepika@gmail.com>
2026-03-04 17:52:39 +0530

    Delete project5/csk directory

--- project5/csk/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CskConfig(AppConfig):
-    name = 'csk'
--- project5/csk/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from csk.views import *
-app_name='anything'
-urlpatterns=[
-    path('captain/',captain,name='captain'),
-]
