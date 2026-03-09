867a30f2e84d50d98f62e2e5e7b6e68c44c99592
Deepika <ragipindideepika@gmail.com>
2026-03-09 15:52:20 +0530

    Add files via upload

---
+++ project10/app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def deepika(request):
+    return render(request,'deepika.html')
+
+def babu(request):
+    return render(request,'babu.html')
